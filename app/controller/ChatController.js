f2c9c38ce22ce89a053cc861da4c678cddb78ebd
hareprananda <52982157+hareprananda@users.noreply.github.com>
2021-02-12 11:17:14 +0800

    jwt authenticated for socket

--- app/controller/ChatController.js
+++ app/controller/ChatController.js
@@ -1,5 +1,8 @@
 const socketio = require("socket.io");
 const CryptoJS = require("crypto-js");
+const jwtAuth = require("socketio-jwt-auth");
+const User = require("../database/models/User.model");
+require("dotenv").config();
 function ChatController(server){
     const io = socketio(server,{
         cors : {
@@ -7,13 +10,44 @@ function ChatController(server){
         }
     });
 
+    io.use(jwtAuth.authenticate({
+        secret : process.env.TOKEN_SECRET,
+        succeedWithoutToken : true,
+        algorithm : "HS256",
+    },async function(payload,done){
+        
+        
+        
+        
+        if(payload && payload.id){
+            let user = await User.findOne({"_id":payload.id}).exec();
+            return done(null,user);
+        }else{
+            return done(null, false, 'user does not exist');
+        }
+
+    }))
+    // io.use(function(socket,next){
+    //     var handshakeData = socket.request;
+    //     console.log(handshakeData._query['payload'])
+    //     next();
+    // })
+
+
+
     io.on("connection",socket => {
         console.log("yuhu ada yang join");
 
+        socket.emit("success",{
+            message : "Success logged in",
+            user : socket.request.user
+        });
+
         socket.on("sendMessage",(payload,callback)=> {
 
-           
-            callback("response dari server");
+            console.log(payload);
+
+            callback(`response dari server, pengirim adalah ${socket.request.user.username} socket id adalah ${socket.id} `);
         })
 
 
